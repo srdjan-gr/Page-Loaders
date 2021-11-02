eb8a57362bb1600a1eac6e0c0ac75f3d059b6c3d
srdjan <srdjan.gr@gmail.com>
2021-11-02 12:50:45 +0100

    Loader-7 js file added.
    JS file is same as tutorial file. Not like in previous Loaders.

--- Loader-6/loader-6.js
+++ Loader-6/loader-6.js
@@ -1,5 +1,5 @@
 
 $(window).on('load',function(){
     $('.init').fadeOut(2000)
-    $('.main').fadeIn(2000)
+    $('.main').fadeIn(1000)
     })
--- Loader-7/loader-7.js
+++
@@ -1,5 +0,0 @@
-
-$(window).on('load',function(){
-    $('.loader').fadeOut(2000)
-    $('.main').fadeIn(2000)
-    })
